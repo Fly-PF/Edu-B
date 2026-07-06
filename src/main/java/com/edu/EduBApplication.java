2ab6f66a64824849e50a245a777cddeaae06b1a3
Fly <782278047@qq.com>
2026-07-06 14:54:17 +0800

    初始项目

---
+++ src/main/java/com/edu/EduBApplication.java
@@ -0,0 +1,13 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduBApplication.class, args);
+    }
+
+}
